a73c1fae666a7d8ea67705ea58eced9f5c83944b
jam <41418470+Sardonyx001@users.noreply.github.com>
2024-11-09 01:29:46 +0900

    feat: :sparkles: add few buttons and labels

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
 import { ReactReader } from "react-reader";
-import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 const App = () => {
   const [location, setLocation] = useState<string | number>(0);
+  const [url, setUrl] = useState("");
+
   return (
     <div>
-      <Button>Click me!</Button>
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <Label htmlFor="picture">Picture</Label>
+        <Input id="picture" type="file" />
+      </div>
       <div style={{ height: "100vh" }}>
         <ReactReader
           url="https://react-reader.metabits.no/files/alice.epub"
